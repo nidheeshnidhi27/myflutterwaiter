ec4fd2c64ba066eebbcd458bb8a3dd33066ead45
nidheeshnidhi27 <48004595+nidheeshnidhi27@users.noreply.github.com>
2026-02-13 12:18:40 +0530

    version cahnge
    logo name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:my_flutter_login/main.dart';
+import 'package:joowaiter/main.dart';
 
 void main() {
   testWidgets('App loads login screen', (tester) async {
